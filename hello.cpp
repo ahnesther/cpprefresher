53c500f0b9627479dc728dd564ddedf51f269fe9
ahnesther <52338697+ahnesther@users.noreply.github.com>
2022-03-02 02:21:50 -0500

    adding encaps

--- hello.cpp
+++ hello.cpp
@@ -6,10 +6,34 @@ using std::endl;
 
 class Employee {
     // properties
-    public:
+    private:
         string Name;
         string Company;
         int Age;
+    public:
+        // setters and getters (ugly)
+        void setName(string name) {
+            this->Name = name;
+        }
+        string getName() {
+            return this->Name;
+        }
+
+        void setCompany(string company) {
+            this->Company = company;
+        }
+        string getCompany() {
+            return this->Company;
+        }
+
+        void setAge(int age) {
+            if(age >= 18) {
+                this->Age = age;
+            }
+        }
+        int getAge() {
+            return this->Age;
+        }
 
         // constructor :hearteyeemoji:
         Employee(string name, string company, int age) {
@@ -25,9 +49,14 @@ class Employee {
 int main() {
     Employee emp1 = Employee("Bill", "Microsoft", 25);
     emp1.Introduce();
+    emp1.setAge(10); // will not change cos of the check.
+    emp1.setCompany("Uber");
+    emp1.Introduce();
+    cout << emp1.getAge();
 
     Employee emp2 = Employee("John", "Amazon", 53);
     emp2.Introduce();
+    cout << emp2.getAge();
 
     return 0;
 }
