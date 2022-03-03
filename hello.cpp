0179a455982a9dbdcdb84bba40ae9b9cc8091293
ahnesther <52338697+ahnesther@users.noreply.github.com>
2022-03-02 22:30:03 -0500

    add inheritance stuffs

--- hello.cpp
+++ hello.cpp
@@ -20,9 +20,11 @@ class AbstractEmployee {
 class Employee:AbstractEmployee {
     // properties
     private:
-        string Name;
         string Company;
         int Age;
+    protected:
+        string Name;
+
     public:
         // setters and getters (ugly)
         void setName(string name) {
@@ -67,6 +69,46 @@ class Employee:AbstractEmployee {
         }
 };
 
+// Developer inherits from Employee.
+// Developer is now a subclass. 
+// Employee is superclass/parentclass/baseclass
+// Developer has all the properties of Employee.
+class Developer:public Employee {
+    private:
+        string FavProgrammingLanguage;
+    public:
+        Developer(string name, string company, int age, string favProgrammingLanguage)
+            :Employee(name, company, age) {
+            this->FavProgrammingLanguage = favProgrammingLanguage;
+        }
+        void setFavLang(string lang) {
+            this->FavProgrammingLanguage = lang;
+        }
+        string getFavLang() {
+            return this->FavProgrammingLanguage;
+        }
+
+        void FixBug() {
+            cout << this->Name << " is fixing a bug with " << this->getFavLang() << endl;
+        }
+};
+
+class Manager:public Employee{
+    public:
+        string Department;
+        int YearsOfExperience;
+        Manager(string name, string company, int age, string department, int yearsOfExperience)
+            :Employee(name, company, age) {
+                this->Department = department;
+                this->YearsOfExperience = yearsOfExperience;
+        }
+        void PreparingMeeting() {
+            cout << this->Name << " is preparing a meeting for the " << this->Department << " all hands deck thing." << endl;
+            cout << "They have used their " << this->YearsOfExperience << " years of work into their meeting well." << endl;
+            cout << "It was a success!" << endl;
+        }
+};
+
 int main() {
     Employee emp1 = Employee("Bill", "Microsoft", 25);
     emp1.Introduce();
@@ -78,5 +120,13 @@ int main() {
     emp1.AskForPrmotion();
     emp2.AskForPrmotion();
 
+    Developer dev = Developer("Harry", "Uber", 28, "Golang");
+    dev.FixBug();
+    dev.AskForPrmotion(); // was not available until we put public after Developer:
+    // we made the inheritance PUBLIC
+
+    Manager mgr = Manager("Nina", "Gurgle", 35, "Research", 10);
+    mgr.PreparingMeeting();
+    mgr.AskForPrmotion();
     return 0;
 }
